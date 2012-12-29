78daf5593f0d3946994168dcd5ead96daa4ab671
David Miller <david@deadpansincerity.com>
2012-12-29 21:28:57 +0000

    initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn zippcare.wsgi --workers=4 --bind=0.0.0.0:$PORT
---
+++ ohcgo/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    # Examples:
+    # url(r'^$', 'ohcgo.views.home', name='home'),
+    # url(r'^ohcgo/', include('ohcgo.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+
+
+
+urlpatterns += staticfiles_urlpatterns()
