86e44d0a48c71f7ce91c1096ef0832194247051b
David Miller <david@deadpansincerity.com>
2013-03-02 16:14:43 +0000

    Add tool categories and dump tools into tool page

--- ohcgo/products/admin.py
+++ ohcgo/products/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from ohcgo.products import models
 
 class ProductAdmin(admin.ModelAdmin):
-    class Meta:
-        model = models.Product
+    """
+    Make products editable
+    """
+    list_display = ('name', 'description', 'image_location', 'category')
+    list_editable = ('description', 'image_location', 'category')
+    list_filter = ('category',)
 
 admin.site.register(models.Product, ProductAdmin)
